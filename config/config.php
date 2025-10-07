fd54fdd1bc9b8f0fe2ef6711b132f3745c1b1267
vanhoa-tdc <hoa96610@gmail.com>
2025-10-07 14:09:35 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "localhost";
+$configDB["host"] 		= "hktweb.mysql.database.azure.com";
 $configDB["database"]	= "computer_store";
-$configDB["username"] 	= "root";
-$configDB["password"] 	= "";
+$configDB["username"] 	= "hktpc_hoa";
+$configDB["password"] 	= "tdc@12345";
 define("HOST", "localhost");
 define("DB_NAME", "computer_store");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_USER", "hktpc_hoa");
+define("DB_PASS", "tdc@12345");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
 define("BASE_URL", "http://".$_SERVER['SERVER_NAME']);//dia chi website
