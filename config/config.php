03cc5832d09591fd290d91678647d33038ca7c49
vanhoa-tdc <hoa96610@gmail.com>
2025-10-07 14:53:27 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,11 +3,11 @@ $configDB = array();
 $configDB["host"] 		= "hktweb.mysql.database.azure.com";
 $configDB["database"]	= "computer_store";
 $configDB["username"] 	= "hktpc_hoa";
-$configDB["password"] 	= "tdc@12345";
+$configDB["password"] 	= "tdc111!!!";
 define("HOST", "localhost");
 define("DB_NAME", "computer_store");
 define("DB_USER", "hktpc_hoa");
-define("DB_PASS", "tdc@12345");
+define("DB_PASS", "tdc111!!!");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
 define("BASE_URL", "http://".$_SERVER['SERVER_NAME']);//dia chi website
